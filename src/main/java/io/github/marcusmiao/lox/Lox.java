d7904ea370dfca82981c4c2a20c0140e51562ff0
marcus-miao <49431129+marcus-miao@users.noreply.github.com>
2025-04-01 20:28:02 +0800

    feat: initial parser implementation

--- src/main/java/io/github/marcusmiao/lox/Lox.java
+++ src/main/java/io/github/marcusmiao/lox/Lox.java
@@ -53,8 +53,16 @@ public class Lox {
     }
   }
 
- public static void error(int line, String message) {
+  static void error(int line, String message) {
     report(line, "", message);
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, "at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
 
   private static void report(int line, String where, String message) {
